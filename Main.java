eef73fd6a4e3ffca1eb01706000ede59f765126d
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2022-07-04 15:48:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+class ExemploList {
+  public static void main(String[] args) {
+    System.out.println("Crie uma lista e adicione as sete notas: ");
+  
+
+
+
+List<Double> notas = new ArrayList<Double>();
+notas.add(7.0);
+notas.add(8.5);
+notas.add(9.3);
+notas.add(5.0);
+notas.add(7.0);
+notas.add(0.0);
+notas.add(7.0);
+notas.add(0.0);
+notas.add(3.6);
+
+ System.out.println(notas.toString());
+
+ System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
+
+ System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+notas.add(4, 8d);
+         
+ System.out.println(notas);
+
+ System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+notas.set(notas.indexof(5d), 6.0);
+
+ System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+
+ /* System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+for (Double nota: notas) System.out.println(nota)*/
+
+  }
+
+}
+            
